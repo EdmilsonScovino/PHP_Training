c3564897e7b6c32cc06652c6d62d3f5e28036865
Edmilson Scovino <56802572+EdmilsonScovino@users.noreply.github.com>
2020-10-22 10:11:43 -0300

    Fim da atividade 03 da Faculdade

--- Faculdade Metodista Granbery/PDF_07_variaveis_e_operadores/atv_3/Atv_3.php
+++ Faculdade Metodista Granbery/PDF_07_variaveis_e_operadores/atv_3/Atv_3.php
@@ -1,8 +1,24 @@
-<?php
-    //Construa um algoritmo que exiba em tela as informações de uma pessoa: Nome, sobrenome, idade, telefone, endereço;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $nome = $_GET["Nome"] ;
+        $sobrenome = $_GET["Sobrenome"];
+        $idade = $_GET["Idade"];
+        $telefone = $_GET["Telefone"];
+        $endereco = $_GET["Endereco"]; 
 
+        echo " O $nome $sobrenome, tem a idade de $idade, o seu telefone para contato <br> (32)$telefone e mora no endereço $endereco ";
     
     
-
-
-?>
+    
+    
+    ?>
+    
+</body>
+</html>
