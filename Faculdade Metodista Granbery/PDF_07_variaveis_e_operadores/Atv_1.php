004e6b28ba21cd5cc61a44474a5d5194e7f245d0
Edmilson Scovino <56802572+EdmilsonScovino@users.noreply.github.com>
2020-10-22 09:35:57 -0300

    Atividades da Faculdade

---
+++ Faculdade Metodista Granbery/PDF_07_variaveis_e_operadores/Atv_1.php
@@ -0,0 +1,14 @@
+<?php
+    //Construa um algoritmo que calcule a área de um círculo
+    
+    $Area = 0;
+    $Valor_p_Calcular = 2 ;
+    $exp = 2;
+
+    
+    $Area = (3.14 * pow ($Valor_p_Calcular, $exp ));
+    
+    echo "Área do círculo foi: $Area";
+
+
+?>
---
+++ Faculdade Metodista Granbery/PDF_07_variaveis_e_operadores/Atv_2.php
@@ -0,0 +1,11 @@
+<?php
+     // Construa um algoritmo que, definido um número, informar se ele é divisível por 2
+
+    $VALOR = 100;
+    if ($VALOR % 2 == 0){
+        echo "O valor e divisivel por 2";
+    }else{
+        echo "O valor não e divisivel por 2";
+    }    
+
+?>
